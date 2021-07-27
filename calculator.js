d32d8bfa4007de0f05bce66dde76f26adf322119
yetiasg <yetiasg@gmail.com>
2021-07-27 17:19:51 +0200

    Done application - full customizable calculator

--- calculator.js
+++ calculator.js
@@ -104,3 +104,115 @@ equalsButton.addEventListener('click', () => {
   calculator.compute();
   calculator.updateOutput();
 });
+
+// Customizer ----------------------------------------
+
+const outputBGCColor = document.getElementById('output--background_color');
+const outputBGText = document.getElementById('output--background_text');
+
+const outputFCColor = document.getElementById('output--font-color_color');
+const outputFCText = document.getElementById('output--font-color_text');
+
+const buttonBGCColor = document.getElementById('buttons--background_color');
+const buttonBGText = document.getElementById('buttons--background_text');
+
+const buttonFCColor = document.getElementById('buttons--font-color_color');
+const buttonFCText = document.getElementById('buttons--font-color_text');
+
+const buttonBGCHoverColor = document.getElementById(
+  'buttons--background--hover_color'
+);
+const buttonBGCHoverText = document.getElementById(
+  'buttons--background--hover_text'
+);
+
+// Change output background color -------------
+
+outputBGCColor.addEventListener('change', () => {
+  const toChange = document.querySelector('.calculator');
+  toChange.style.backgroundColor = outputBGCColor.value;
+  outputBGText.value = outputBGCColor.value;
+});
+
+outputBGText.addEventListener('change', () => {
+  const toChange = document.querySelector('.calculator');
+  toChange.style.backgroundColor = outputBGText.value.toString();
+  outputBGCColor.value = outputBGText.value;
+});
+
+// Change output font color ----------------
+
+outputFCColor.addEventListener('change', () => {
+  const toChange = document.querySelector('.output');
+  toChange.style.color = outputFCColor.value.toString();
+  outputFCText.value = outputFCColor.value;
+});
+
+outputFCText.addEventListener('change', () => {
+  const toChange = document.querySelector('.output');
+  toChange.style.color = outputFCText.value.toString();
+  outputFCColor.value = outputFCText.value;
+});
+
+// Change button background color--------------------------
+
+buttonBGCColor.addEventListener('change', () => {
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach((btn) => {
+    btn.style.backgroundColor = buttonBGCColor.value.toString();
+  });
+  buttonBGText.value = buttonBGCColor.value;
+});
+
+buttonBGText.addEventListener('change', () => {
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach((btn) => {
+    btn.style.backgroundColor = buttonBGText.value.toString();
+  });
+  buttonBGCColor.value = buttonBGText.value;
+});
+
+// Change button font color -----------------------
+
+buttonFCColor.addEventListener('change', () => {
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach((btn) => {
+    btn.style.color = buttonFCColor.value.toString();
+  });
+  buttonFCText.value = buttonFCColor.value;
+});
+
+buttonFCText.addEventListener('change', () => {
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach((btn) => {
+    btn.style.color = buttonFCText.value.toString();
+  });
+  buttonFCColor.value = buttonFCText.value;
+});
+
+// Change background on hover -----------------------
+let hoverColor = '#046b9f';
+buttonBGCHoverColor.addEventListener('change', () => {
+  hoverColor = buttonBGCHoverColor.value.toString();
+  buttonBGCHoverText.value = buttonBGCHoverColor.value;
+});
+
+buttonBGCHoverText.addEventListener('change', () => {
+  hoverColor = buttonBGCHoverText.value.toString();
+  buttonBGCHoverColor.value = buttonBGCHoverText.value;
+});
+
+const buttons = document.querySelectorAll('button');
+let previousBGCColor;
+buttons.forEach((btn) => {
+  btn.addEventListener('mouseover', () => {
+    previousBGCColor = btn.style.backgroundColor;
+    btn.style.backgroundColor = hoverColor;
+  });
+});
+
+buttons.forEach((btn) => {
+  btn.addEventListener('mouseout', () => {
+    btn.style.backgroundColor = previousBGCColor;
+  });
+});

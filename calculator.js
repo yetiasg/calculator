ad2409f57bd6661a85b74697cbc7fb2c80896328
yetiasg <yetiasg@gmail.com>
2021-07-27 13:13:57 +0200

    Calculator ready

--- calculator.js
+++ calculator.js
@@ -13,42 +13,94 @@ class Calculator {
   constructor(previousTextElement, currentTextElement) {
     this.previousTextElement = previousTextElement;
     this.currentTextElement = currentTextElement;
+    this.operation = '';
+    this.fetchedValue = '';
+    this.previousFetchedValue = '';
 
-    // this.clear();
+    this.clear();
   }
 
   clear() {
     this.previousTextElement.innerHTML = '';
     this.currentTextElement.innerHTML = '0';
+    this.operation = '';
+    this.fetchedValue = '';
+    this.previousFetchedValue = '';
   }
 
-  clearAll() {}
+  delete() {
+    this.fetchedValue = this.fetchedValue.slice(0, -1);
+  }
 
-  delete() {}
-
-  fetchNumber(number) {
-    console.log(Number(number));
+  fetchValue(number) {
+    if (number === '.' && this.fetchedValue.includes('.')) return;
+    this.fetchedValue = this.fetchedValue.toString() + number.toString();
   }
 
   chooseOperation(operation) {
-    console.log(operation);
+    if (this.fetchedValue === '') return;
+    if (this.previousFetchedValue !== '') this.compute();
+    this.operation = operation;
+    this.previousFetchedValue = this.fetchedValue;
+    this.fetchedValue = '';
   }
 
-  compute() {}
+  compute() {
+    let computation;
+    const previousValue = parseFloat(this.previousFetchedValue);
+    const currenValue = parseFloat(this.fetchedValue);
+    if (isNaN(previousValue) || isNaN(currenValue)) return;
+    switch (this.operation) {
+      case '+':
+        computation = previousValue + currenValue;
+        break;
+      case '-':
+        computation = previousValue - currenValue;
+        break;
+      case '*':
+        computation = previousValue * currenValue;
+        break;
+      case '/':
+        computation = previousValue / currenValue;
+    }
+    this.fetchedValue = computation;
+    this.previousFetchedValue = '';
+  }
 
-  updateOutput() {}
+  updateOutput() {
+    this.currentTextElement.innerText = this.fetchedValue;
+    this.previousTextElement.innerText =
+      this.previousFetchedValue + ' ' + this.operation;
+  }
 }
 
 const calculator = new Calculator(previousTextElement, currentTextElement);
 
 numberBtns.forEach((btn) => {
   btn.addEventListener('click', () => {
-    calculator.fetchNumber(btn.innerText);
+    calculator.fetchValue(btn.innerText);
+    calculator.updateOutput();
   });
 });
 
 operationBtns.forEach((btn) => {
   btn.addEventListener('click', () => {
     calculator.chooseOperation(btn.innerText);
+    calculator.updateOutput();
   });
+});
+
+clearBtn.addEventListener('click', () => {
+  calculator.clear();
+  calculator.updateOutput();
+});
+
+deleteBtn.addEventListener('click', () => {
+  calculator.delete();
+  calculator.updateOutput();
+});
+
+equalsButton.addEventListener('click', () => {
+  calculator.compute();
+  calculator.updateOutput();
 });
